42d4c1eed6d6bc0ac293ead712b02f07b360b6be
maxim <m.bobens@htlkrems.at>
2024-05-26 22:21:47 +0200

    initial commit

---
+++ Pages/BookingModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorApp2.Pages;
+
+public class BookingModel
+{
+    public string TraineeName { get; set; } = string.Empty;
+    public string Station { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public string Status { get; set; } = string.Empty;
+}
